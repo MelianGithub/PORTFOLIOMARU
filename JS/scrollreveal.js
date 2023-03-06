2027cb4f83114bcbe0d04716dee4d2bca81080d5
MelianGithub <e_melian04@hotmail.com>
2023-03-06 17:22:28 -0300

    changing background about

---
+++ JS/scrollreveal.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".reveal", {
+    delay: 120, // Retrasar la animación 200ms
+    duration: 700, // Duración de la animación de 1 segundo
+    easing: "ease-out", // Añadir un efecto de aceleración a la animación
+    origin: "bottom", // Hacer que los elementos se muestren desde la parte inferior de la página
+    distance: "30px", // Distancia desde donde los elementos aparecerán
+    // reset:true,
+    viewFactor: 0,
+    opacity: 0
+
+})
+sr.reveal(".reveal2", {
+    delay: 100, // Retrasar la animación 200ms
+    duration: 700, // Duración de la animación de 1 segundo
+    easing: "ease-out", // Añadir un efecto de aceleración a la animación
+    origin: "bottom", // Hacer que los elementos se muestren desde la parte inferior de la página
+    distance: "40px", // Distancia desde donde los elementos aparecerán
+    // reset:true,
+    viewFactor: 0,
+
+})
+
+
+
