0e417f11c28c1910c485b8a09fc193482b16bfca
MelianGithub <e_melian04@hotmail.com>
2023-03-10 17:49:35 -0300

    getting ready 10

--- JS/scrollreveal.js
+++ JS/scrollreveal.js
@@ -21,6 +21,7 @@ sr.reveal(".reveal2", {
     viewFactor: 0,
 
 })
+
 sr.reveal(".reveal3", {
     delay: 400, // Retrasar la animación 200ms
     duration: 700, // Duración de la animación de 1 segundo
@@ -29,28 +30,45 @@ sr.reveal(".reveal3", {
     distance: "40px", // Distancia desde donde los elementos aparecerán
     // reset:true,
     viewFactor: 0,
-
-})
-sr.reveal(".reveal-left", {
-    delay: 200, // Retrasar la animación 200ms
-    duration: 700, // Duración de la animación de 1 segundo
-    easing: "ease-out", // Añadir un efecto de aceleración a la animación
-    origin: "left", // Hacer que los elementos se muestren desde la parte inferior de la página
-    distance: "40px", // Distancia desde donde los elementos aparecerán
-    // reset:true,
-    viewFactor: 0,
-
-})
-sr.reveal(".reveal-right", {
-    delay: 200, // Retrasar la animación 200ms
-    duration: 700, // Duración de la animación de 1 segundo
-    easing: "ease-out", // Añadir un efecto de aceleración a la animación
-    origin: "right", // Hacer que los elementos se muestren desde la parte inferior de la página
-    distance: "40px", // Distancia desde donde los elementos aparecerán
-    // reset:true,
-    viewFactor: 0,
-
 })
 
+if (window.innerWidth <= 500) {
+    sr.reveal(".reveal-left", {
+        delay: 200, 
+        duration: 700,
+        easing: "ease-out",
+        origin: "bottom",
+        distance: "40px",
+        viewFactor: 0,
+    })
+} else {
+    sr.reveal(".reveal-left", {
+        delay: 200,
+        duration: 700,
+        easing: "ease-out",
+        origin: "left",
+        distance: "40px",
+        viewFactor: 0,
+    })
+}
 
+if (window.innerWidth <= 500) {
+    sr.reveal(".reveal-right", {
+        delay: 200,
+        duration: 700,
+        easing: "ease-out",
+        origin: "bottom",
+        distance: "40px",
+        viewFactor: 0,
+    })
+} else {
+    sr.reveal(".reveal-right", {
+        delay: 200,
+        duration: 700,
+        easing: "ease-out",
+        origin: "right",
+        distance: "40px",
+        viewFactor: 0,
+    })
+}
 
